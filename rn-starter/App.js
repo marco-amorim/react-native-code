0ac094dc0d5aa248e48b2c4daa1d01df5675c6c4
marco-amorim <marcoamorim95@hotmail.com>
2021-01-11 09:16:26 -0300

    showing custom component

--- rn-starter/App.js
+++ rn-starter/App.js
@@ -1,17 +1,19 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import ComponentScreen from './src/screens/ComponentScreen';
 import HomeScreen from './src/screens/HomeScreen';
 
 const navigator = createStackNavigator(
-  {
-    Home: HomeScreen,
-  },
-  {
-    initialRouteName: 'Home',
-    defaultNavigationOptions: {
-      title: 'App',
-    },
-  }
+	{
+		Home: HomeScreen,
+		Components: ComponentScreen,
+	},
+	{
+		initialRouteName: 'Components',
+		defaultNavigationOptions: {
+			title: 'App',
+		},
+	}
 );
 
 export default createAppContainer(navigator);
